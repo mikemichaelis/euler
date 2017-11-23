b67483904a22c112e0d94a058dd33dd77f2fa187
Charlie <charlie.michaelis@gmail.com>
2017-11-22 20:17:48 -0600

    Add Problem 5

---
+++ 5/main.py
@@ -0,0 +1,20 @@
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+# going the easy string route...easy to read, easy to maintain, easy to understand, easy to code.....
+def isPalindrome(n):
+    m = str(n)
+    return m == m[::-1]
+
+
+largest = 0
+# start with large numbers first to exclude calling isPalindrome() for smaller products
+for a in range(999, 99, -1):
+    for b in range(999, 99, -1):
+        product = a * b
+        # check the product is larger than current largest
+        if(product > largest):
+            if(isPalindrome(product) == True):
+                largest = product
+
+print(largest)
